1b74972d33d1c1c189a487bf8d591c3e05361fde
olibanovip <leon4rdo@gmail.com>
2025-02-26 21:49:09 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,46 @@
-const CACHE_NAME = 'olibano-v2';
+const CACHE_NAME = 'olibano-v3';
+const OFFLINE_URL = '/files/install.html';
 const urlsToCache = [
-    '/files/',
-    '/files/index.html',
-    '/files/manifest.json',
-    '/files/icon-192.png',
-    '/files/icon-512.png',
-    '/files/install.html'
+  '/files/',
+  '/files/install.html',
+  '/files/manifest.json',
+  '/files/icon-192.png',
+  '/files/icon-512.png',
+  'https://airgo.bio/olibano' // Cache da URL final
 ];
 
 self.addEventListener('install', event => {
-    console.log('Service Worker instalando...');
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('fetch', event => {
-    console.log('Fetch interceptado:', event.request.url);
+  if (event.request.mode === 'navigate') {
     event.respondWith(
-        caches.match(event.request)
+      fetch(event.request)
+        .catch(() => caches.match(OFFLINE_URL))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
