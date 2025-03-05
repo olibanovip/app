b1c4ac2918387aa963525769840ec5e4815c058b
olibanovip <leon4rdo@gmail.com>
2025-03-05 18:56:30 +0000

    Update and rename sw.js to service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,5 +7,5 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Requisitando:', event.request.url);
+  event.respondWith(fetch(event.request));
 });
